992aadfd3a58a5433c2e373841cab521a28c1b8f
Sneha Tiwari <48124160+Ohhmydude@users.noreply.github.com>
2021-08-20 18:23:24 +0530

    Add files via upload

---
+++ node.py
@@ -0,0 +1,150 @@
+from types import MethodDescriptorType
+from flask import Flask, jsonify, request
+from wallet import Wallet
+from flask_cors import CORS
+from blockchain import Blockchain
+
+app = Flask(__name__)
+wallet = Wallet()
+blockchain = Blockchain(wallet.public_key)
+CORS(app)
+
+
+@app.route('/wallet', methods=['POST'])
+def create_keys():
+    wallet.create_keys()
+    if wallet.save_keys():
+        
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        response ={
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'saving key fail'
+        }
+    return jsonify(response), 500
+
+@app.route('/wallet', methods= ['GET'])
+def load_keys():
+    if wallet.load_keys():
+        
+        global blockchain
+        blockchain = Blockchain(wallet.public_key)
+        response ={
+            'public_key': wallet.public_key,
+            'private_key': wallet.private_key,
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'loading key fail'
+        }
+    return jsonify(response), 500
+
+@app.route('/balance', methods=['GET'])
+def get_balance():
+    balance = blockchain.get_balance()
+    if balance != None:
+        response = {
+            'message': 'Fetched balance succesfully',
+            'funds': balance
+        }
+        return jsonify(response), 200
+    else:
+        response = {
+            'message': 'Loading balance fails',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+@app.route('/', methods=['GET'])
+def get_ui():
+    return 'wohoo!'
+
+
+@app.route('/transaction', methods = ['POST'])
+def add_transaction():
+    if wallet.public_key == None:
+        response = {
+            'message':'No wallet setup'
+        }
+        return jsonify(response), 400
+
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': 'no data found'
+        }
+        return jsonify(response), 400
+    required_fields = ['recipient', 'amount']
+    if not all(field in values for field in required_fields):
+        response = {
+            'message': 'required data missing'
+        }
+        return jsonify(response), 400
+    recipient = values['recipient']
+    amount = values['amount']
+    if blockchain.add_transaction(recipient, wallet.public_key, amount):
+        response = {
+            'message':'Successfully added transaction',
+            'transaction': {
+                'sender': wallet.public_key,
+                'recipient': recipient,
+                'amount': amount,
+            },
+            'funds': blockchain.get_balance()
+        }
+        return jsonify(response), 201
+
+    else:
+        response={
+            'message':'Creating a transaction failed'
+        }
+        return jsonify(response), 500
+    
+
+
+
+
+@app.route('/mine', methods=['POST'])
+def mine():
+    block = blockchain.mine_block()
+    if block != None:
+        dict_block = block.__dict__.copy()
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+        response = {
+            'message':'Block added succesfully',
+            'block': dict_block,
+            'funds': blockchain.get_balance()
+
+        }
+        return jsonify(response), 201
+    else:
+        response = {
+            'message': 'Adding a block failed',
+            'wallet_set_up': wallet.public_key != None
+        }
+        return jsonify(response), 500
+
+
+
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_snapshot = blockchain.chain
+    dict_chain = [block.__dict__.copy() for block in chain_snapshot]
+    for dict_block in dict_chain:
+        dict_block['transactions'] = [tx.__dict__ for tx in dict_block['transactions']]
+    return jsonify(dict_chain), 200
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port =5000)
